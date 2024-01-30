e0133717a821bb3f7825d82790d0f85bdd34c052
Jon Max <jonmax.hu@farben.com.cn>
2024-01-30 19:02:33 +0800

    consumer-commit

---
+++ eureka-demo/service-consumer/src/main/java/com/jon/max/pojo/Product.java
@@ -0,0 +1,16 @@
+package com.jon.max.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+    private Integer id;
+    private String productName;
+    private Integer productNum;
+    private double productPrice;
+}
